e303d8cf7bf183a66d6b186e24cb91702d24b12c
altersaber <altersaber26@gmail.com>
2022-01-10 22:04:16 +0200

    Added functionality to Edit and Remove buttons

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const form = document.querySelector("form")
 const input = document.querySelector("[name='todo']")
-const todoList = document.querySelector(".todos")
+const todoList = document.querySelector("#todolist")
 
 const todoItems = []
+const todoStrings = []
 
 function addTodo(todoText) {
 
-    // Creating new Item (container)
+    // Creating new Item (and textContainer inside)
     const newItem = document.createElement("div")
-    newItem.innerText = todoText
+    const textContainer = document.createElement("div")
+    textContainer.classList.add("text-container")
+    textContainer.innerText = todoText
+    newItem.appendChild(textContainer)
     newItem.classList.add("new-item")
+    todoList.classList.add("todos")
 
 
     // Creating Edit button
     const newItemEditForm = document.createElement("form")
+    newItemEditForm.classList.add("buttons-form")
     const newItemEdit = document.createElement("label")
-
+    newItemEdit.classList.add("buttons-label")
+    // Edit button itself (will be hidden via CSS inside the label)
     const newItemEditButton = document.createElement("input")
     newItemEditButton.setAttribute("type", "submit")
     newItemEditButton.classList.add("new-item-edit-button")
-
+    // SVG to cover the Edit button
     const newItemEditSVG = document.createElement("img")
     newItemEditSVG.setAttribute("src", "svg/iconmonstr-pencil-14.svg")
     newItemEditSVG.classList.add("new-item-edit-svg")
@@ -30,40 +37,66 @@ function addTodo(todoText) {
 
     // Creating Remove button
     const newItemRemoveForm = document.createElement("form")
+    newItemRemoveForm.classList.add("buttons-form")
     const newItemRemove = document.createElement("label")
-
+    newItemRemove.classList.add("buttons-label")
+    // Remove buttom itself (will be hidden via CSS inside the label)
     const newItemRemoveButton = document.createElement("input")
     newItemRemoveButton.setAttribute("type", "submit")
     newItemRemoveButton.classList.add("new-item-remove-button")
-
+    // SVG to cover the Remove button
     const newItemRemoveSVG = document.createElement("img")
     newItemRemoveSVG.setAttribute("src", "svg/12555439561530177266.svg")
-    newItemRemoveSVG.setAttribute("width", "25px")
+    newItemRemoveSVG.setAttribute("width", "24px")
     newItemRemoveSVG.classList.add("new-item-remove-svg")
     newItemRemove.append(newItemRemoveButton, newItemRemoveSVG)
     newItemRemoveForm.appendChild(newItemRemove)
     newItemRemoveForm.addEventListener("submit", removeItem)
 
-    // Appending form -> item -> todoList
+
+    // Creating container for both buttons
     const buttonsContainer = document.createElement("div")
     buttonsContainer.classList.add("buttons-container")
     buttonsContainer.append(newItemEditForm, newItemRemoveForm)
     newItem.appendChild(buttonsContainer)
 
+    // Add Item to the array todoItems as an object
     todoItems.push(newItem)
+    // Add Item to the array todoStrings as a string
+    todoStrings.push(todoText)
     
     todoList.appendChild(newItem)
 
+    // Callback for Removing Item
     function removeItem(event) {
         event.preventDefault()
         newItem.remove()
     }
+    // Callback for Editing Item
     function editItem(event) {
         event.preventDefault()
+        // Creating the warning message after ToDo's text
+        const warningSpan = document.createElement("span")
+        warningSpan.innerText = "Press <Enter> to save"
+        warningSpan.classList.add("warning-span")
+        textContainer.appendChild(warningSpan)
+
+        textContainer.setAttribute("contenteditable", "true")
+        textContainer.addEventListener("keydown", e => {
+            // Pressing Enter verifies the input and prevents from futher editing
+            if (e.keyCode == 13) {
+                textContainer.removeAttribute("contenteditable")
+                // Removing warning message
+                warningSpan.remove()
+            }
+        })
     }
 }
-
+// Submitting form calls addTodo() if the current value is not an empty string
 form.onsubmit = (event) => {
     event.preventDefault()
-    addTodo(input.value)
+    if (input.value !== "") {
+        addTodo(input.value)
+    }
+    input.value = ""
 }
